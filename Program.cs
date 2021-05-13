6f9a3a3cb03e1a42cbc84b650c53c62c8ecb16b2
吉本 幸志郎 <010382@CHIIKIKAGAKU.local>
2021-05-13 15:17:10 +0900

    edit 20210513

--- Program.cs
+++ Program.cs
@@ -7,132 +7,152 @@ namespace Training1
         static void Main()
         {
             var list = new LinkedList();
-            list.Add(new StringNode("A"));
-            list.Remove("A");
-            // list.Add(new StringNode("B"));
-            // list.Add(new StringNode("C"));
-            // list.Add(new StringNode("D"));
-            // list.Add(new StringNode("E"));
-            // list.Insert(0, new StringNode("!!"));
-            // Console.WriteLine(list.ToString());
-            // list.RemoveAt(3);
-            // Console.WriteLine(list.ToString());
-            // list.Add(new StringNode("??"));
-            // Console.WriteLine(list.ToString());
-            // list.Remove("D");
-            // Console.WriteLine(list.ToString());
+            list.AddLast(new StringNode("A"));
+            // list.Remove("A");
+            list.AddLast(new StringNode("B"));
+            list.AddLast(new StringNode("C"));
+            list.AddLast(new StringNode("D"));
+            list.AddLast(new StringNode("E"));
+            list.Insert(5, new StringNode("!!"));
+
+            list.Insert(1, new StringNode("!!"));
+            list.Insert(0, new StringNode("!!"));
+
+            Console.WriteLine(list.ToString());
+            list.RemoveAt(0);
+            Console.WriteLine(list.ToString());
+            list.AddLast(new StringNode("??"));
+            Console.WriteLine(list.ToString());
+            bool result = list.Remove("??S");
+            if (result == false)
+            {
+                Console.WriteLine("Could not remove!");
+            }
+            Console.WriteLine(list.ToString());
+            list.Clear();
+            Console.WriteLine(list.ToString());
         }
 
     }
 
-    class LinkedList {
+    class LinkedList
+    {
 
         public StringNode Head { get; private set; }
-        public StringNode Tail { get; private set; }
-        public LinkedList(){}
-
-        private void ConnectNode(StringNode prevNode, StringNode nextNode)
+        public LinkedList()
         {
-            //Connect new node to next node
-            if (nextNode != null) {
-                nextNode.Prev = prevNode;
-            }
-            prevNode.Next = nextNode;
         }
 
-        private StringNode GetAt(int index) 
+        private StringNode GetAt(int index)
         {
-            StringNode currentNode = this.Head;
-            int i = 0;
-            while(currentNode != null) {
-                if (i == index) return currentNode;
-                currentNode = currentNode.Next;
-                i++;
+            if (index < 0 || Count < index) throw new ArgumentOutOfRangeException("index");
+
+            StringNode current = this.Head;
+            for (int i = 0; i < index; i++)
+            {
+                current = current.Next;
             }
-            throw new ArgumentOutOfRangeException();
+            return current;
         }
 
         private int IndexOf(StringNode node)
         {
+            StringNode current = this.Head;
             int index = 0;
-            StringNode currentNode = this.Head;
-            while (currentNode != null) {
-                if (currentNode.Equals(node)) return index;
+            while (index < Count)
+            {
+                if (current.Equals(node)) return index;
+                current = current.Next;
                 index++;
-                currentNode = currentNode.Next;
             }
             return -1;
         }
 
-        public void Add(StringNode node)
+        public void AddFirst(StringNode node)
         {
-            if (this.Head == null) { 
-                this.Head = node;
-                this.Count = 1;
-                return;
+            ValidateNewNode(node);
+
+            if (Head == null)
+            {
+                InternalInsertNodeToEmptyList(node);
             }
-            // Get tail node
-            StringNode tailNode = GetAt(Count-1);
-            //Connect node to tailNode
-            ConnectNode(tailNode, node);
-            this.Count++;
+            else
+            {
+                InternalInsertNodeBefore(Head, node);
+                Head = node;
+            }
+        }
+        public void AddLast(StringNode node)
+        {
+            ValidateNewNode(node);
+
+            if (Head == null)
+            {
+                InternalInsertNodeToEmptyList(node);
+            }
+            else
+            {
+                InternalInsertNodeBefore(Head, node);
+            }
+        }
+        public void Insert(int index, StringNode newNode)
+        {
+            if (index < 0 || Count < index) throw new IndexOutOfRangeException();
+
+            ValidateNewNode(newNode);
+            // Insert node into head
+            if (index == 0)
+            {
+                AddFirst(newNode);
+            }
+            else
+            {
+                StringNode node = GetAt(index);
+                InternalInsertNodeBefore(node, newNode);
+            }
         }
 
-        public void Insert(int index, StringNode node)
-        {
-            // Insert node into Head
-            if (index == 0) { 
-                node.Next = this.Head;
-                if(this.Head != null) {
-                    this.Head.Prev = node;
-                }
-                // Replace 
-                this.Head = node;
-                this.Count++;
-                return;
-            }
-            // Get previous node of index
-            StringNode prevNode = GetAt(index-1);
-            // Out of index
-            if (prevNode == null) return;
-            StringNode nextNode = prevNode.Next;
-            ConnectNode(node, nextNode);
-            ConnectNode(prevNode, node);
-            this.Count++;
-        }
-
-        public void Remove(string data)
+        public bool Remove(string data)
         {
             int index = IndexOf(new StringNode(data));
-            RemoveAt(index);
+
+            if (index == -1)
+            {
+                return false;
+            }
+            else
+            {
+                bool result = RemoveAt(index);
+                return result;
+            }
         }
 
-        public void RemoveAt(int index)
+        public bool RemoveAt(int index)
         {
-            // Rmove node from Head
-            if (index == 0) { 
-                if (this.Head.Next != null) {
-                    this.Head.Prev = null;
-                }
-                this.Head = this.Head.Next;
-                this.Count--;
-                return;
+            StringNode node;
+            try
+            {
+                node = GetAt(index);
             }
-            StringNode currentNode = GetAt(index);
-            if(currentNode == null) return;
-            ConnectNode(currentNode.Prev, currentNode.Next);
-            this.Count--;
+            catch
+            {
+                return false;
+            }
+            InternalRemoveNode(node);
+            return true;
         }
 
         public void Clear()
         {
-            var currentNode = this.Head;
-            this.Head = null;
-            while (currentNode != null) {
-                currentNode = currentNode.Next;
-                currentNode.Next = null;
-                currentNode.Prev = null;
+            var current = this.Head;
+            while (current != null)
+            {
+                StringNode temp = current;
+                current = current.Next;
+                temp.Invalidate();
             }
+            Head = null;
+            Count = 0;
         }
 
         // public int Count {get { return 0; }}
@@ -142,21 +162,66 @@ namespace Training1
         {
             var sb = new System.Text.StringBuilder();
             var currentNode = this.Head;
-            while(currentNode != null) {
+            for (int i = 0; i < Count; i++)
+            {
                 sb.Append($"[{currentNode.Data}]->");
                 currentNode = currentNode.Next;
-                }
-            sb.Append("[end]");            
+            }
+            sb.Append("[end]");
             return sb.ToString();
         }
+
+        internal void InternalInsertNodeToEmptyList(StringNode newNode)
+        {
+            newNode.Next = newNode;
+            newNode.Prev = newNode;
+            Head = newNode;
+            Count = 1;
+        }
+
+        internal void InternalInsertNodeBefore(StringNode node, StringNode newNode)
+        {
+            newNode.Next = node;
+            newNode.Prev = node.Prev;
+            node.Prev.Next = newNode;
+            node.Prev = newNode;
+            Count++;
+        }
+
+        internal void InternalRemoveNode(StringNode node)
+        {
+            if (node.Next == node)
+            {
+                Head = null;
+            }
+            else
+            {
+                node.Next.Prev = node.Prev;
+                node.Prev.Next = node.Next;
+                if (Head == node)
+                {
+                    Head = node.Next;
+                }
+            }
+            node.Invalidate();
+            Count--;
+        }
+        internal void ValidateNewNode(StringNode node)
+        {
+            if (node == null)
+            {
+                throw new ArgumentNullException();
+            }
+        }
     }
-    
-    class StringNode {
+
+    class StringNode
+    {
         public string Data { get; private set; }
         public StringNode Next { get; set; }
         public StringNode Prev { get; set; }
 
-        public StringNode(string data) 
+        public StringNode(string data)
         {
             this.Data = data;
         }
@@ -168,25 +233,35 @@ namespace Training1
 
         public override bool Equals(object obj)
         {
-            if(this == obj) {
+            if (this == obj)
+            {
                 return true;
             }
 
-            if(!(obj is StringNode)) {
+            if (!(obj is StringNode))
+            {
                 return false;
             }
 
             var other = obj as StringNode;
 
-            if(this.GetHashCode()!=other.GetHashCode()) {
+            if (this.GetHashCode() != other.GetHashCode())
+            {
                 return false;
             }
 
-            if(!(this.Data.Equals(other.Data))) {
+            if (!(this.Data.Equals(other.Data)))
+            {
                 return false;
             }
 
             return true;
+        }
+
+        internal void Invalidate()
+        {
+            Next = null;
+            Prev = null;
         }
 
         public override string ToString()
